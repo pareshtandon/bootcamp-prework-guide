3ee19c7eef58ca1712d512f63122b15ec79ed9b4
pareshtandon <pareshtandon@hotmail.com>
2023-02-19 21:55:40 -0600

    Add for loop and conditional statement in javascript

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,49 @@
 var topic = ["HTML","Git","JavaScript","CSS",""];
-//console.log(topic[0]);
 
-for (var i=0; i< topic.length; i++) {
-    if (topic[i] === "HTML"){
+var randomTopic = topic[Math.floor(Math.random() *topic.length)]
+console.log(randomTopic);
+
+
+/*function selectTopic() {
+    for (var i=0; i< topic.length; i++) {
+        if (topic[i] === "HTML"){
+            console.log("Let's study HTML!");
+        } else if (topic[i] === "CSS"){
+            console.log("Let's study CSS!");
+        } else if (topic[i] === "Git"){
+            console.log("Let's study Git!");
+        } else if (topic[i] === "JavaScript"){
+            console.log("Let's study JavaScript!");
+        } else {
+            console.log("Please try again later!");
+        }    
+    }
+}*/
+
+function selectTopic() {
+    
+    if (randomTopic === "HTML"){
         console.log("Let's study HTML!");
-    } else if (topic[i] === "CSS"){
+    } else if (randomTopic === "CSS"){
         console.log("Let's study CSS!");
-    } else if (topic[i] === "Git"){
+    } else if (randomTopic === "Git"){
         console.log("Let's study Git!");
-    } else if (topic[i] === "JavaScript"){
+    } else if (randomTopic === "JavaScript"){
         console.log("Let's study JavaScript!");
     } else {
         console.log("Please try again later!");
+    }    
+    
+}
+
+function listTopics() {
+    for (var i=0; i< topic.length; i++) {
+        console.log("Lets study " + topic[i] + " and enjoy");
     }
 
 }
-
-for (var i=0; i< topic.length; i++) {
-   console.log("Lets study " + topic[i] + " and enjoy")
-
-}
-
+console.log("Here are the topics we learned through prework");
+listTopics();
+console.log("Which topic we should study first");
+selectTopic();
 
